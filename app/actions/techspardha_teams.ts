8782bb6532812825d44532fe8b6ebba3af6c5697
Debatreya <debatreyadas@gmail.com>
2025-01-10 03:10:25 +0530

    feat(techspardha_teams): Intermediate commit

--- app/actions/techspardha_teams.ts
+++ app/actions/techspardha_teams.ts
@@ -42,3 +42,38 @@ enum Post {
 	Convenor = "Convenor",
 	CoConvenor = "Co-Convenor",
 }
+
+export type TechspardhaTeamsDTO = {
+
+}
+
+export type TechspardhaTeamDTO = {
+    
+}
+
+// ACTIONS
+
+// getAll Teams
+export async function getAllTechspardhaTeams(){
+    try {
+
+        // get the collection
+        const teamsCollection = collection(db, "techspardha_teams");
+        
+        // get all the documents
+        const snapshot = await getDocs(teamsCollection);
+
+        // map the data
+        const teams = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
+
+        // Return the teams
+        return teams;
+        
+    } catch (error) {
+        console.error("Error fetching teams:", error);
+        throw error;
+    }
+}
