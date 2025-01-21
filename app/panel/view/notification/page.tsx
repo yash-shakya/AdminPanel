708886b78ce780c920cec96f48c7c2c99246c05b
Swastik19Nit <122457803+Swastik19Nit@users.noreply.github.com>
2025-01-21 18:16:14 +0530

    Update and view notification added , Bug fixed

--- app/panel/view/notification/page.tsx
+++ app/panel/view/notification/page.tsx
@@ -1,15 +1,13 @@
-import CreateForm from "@/app/ui/notification/createForm";
+"use client";
 
-export default async function Notifications() {
-  return (
-    <>
-      <div className="bg-gray-900 p-4 shadow-md rounded-md">
-        <h1 className="border-b pb-2 text-3xl font-black font-mono border-blue-200">
-          Notifications
-        </h1>
-        <h3 className="text-2xl py-2 font-bold font-mono"> Add Notifications </h3>
-        <CreateForm />
-      </div>
-    </>
-  );
+import UserTable from "@/app/ui/admin_users/UserTable"
+import NotificationTable from "@/app/ui/notification/NotificationTable";
+
+export default function Home() {
+    return (
+        <div className="container mx-auto py-6">
+          <h1 className="text-2xl font-bold mb-4">Notifications</h1>
+          <NotificationTable />
+        </div>
+      );
 }
--- app/ui/base_form.tsx
+++ app/ui/base_form.tsx
@@ -1,161 +1,160 @@
-// Purpose: Contains the base form component that all forms will inherit from.
-// All other forms will just pass props to this component. to be rendered.
-
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export interface BaseFormProps {
-	title: string;
-	fields: Field[];
-	submit: any; // function to call when form is submitted (optional)
-	submitText: string;
+  title: string;
+  fields: Field[];
+  submit: any;
+  submitText: string;
 }
 
 interface Field {
-	name: string;
-	label: string;
-	type: "select" | "file" | "text" | "textarea" | "password" | "email" | "date" | "time" | "datetime-local" | "number" | "checkbox";
-	// options : in case of select
-	options?: string[];
-	// placeholder : in case of input
-	placeholder?: string;
-	// required field optional
-	required?: boolean;
-	// default value optional (used in case of update)
-	value?: string;
+  name: string;
+  label: string;
+  type: "select" | "file" | "text" | "textarea" | "password" | "email" | "date" | "time" | "datetime-local" | "number" | "checkbox";
+  options?: string[];
+  placeholder?: string;
+  required?: boolean;
+  value?: string;
 }
 
 export const BaseForm: React.FC<BaseFormProps> = ({
-	title,
-	fields,
-	submit,
-	submitText,
+  title,
+  fields,
+  submit,
+  submitText,
 }) => {
-	const [form, setForm] = useState<Record<string, any>>(
-		fields.reduce((acc, field) => {
-			acc[field.name] = field.value || "";
-			return acc;
-		}, {} as Record<string, any>)
-	);
 
-	const [success, setSuccess] = useState<string>("");
-	const [error, setError] = useState<string>("");
+  const [_, setInitialValues] = useState<Record<string, any>>({});
+  const [form, setForm] = useState<Record<string, any>>({});
+  useEffect(() => {
+    const values = fields.reduce((acc, field) => {
+      acc[field.name] = field.value || "";
+      return acc;
+    }, {} as Record<string, any>);
+    
+    setInitialValues(values);
+    setForm(values);
+  }, [fields]);
 
-	const handleChange = (
-		e: React.ChangeEvent<
-			HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
-		>
-	) => {
-		const { name, value, type, files } = e.target as HTMLInputElement;
+  const [success, setSuccess] = useState<string>("");
+  const [error, setError] = useState<string>("");
 
-		if (type === "file" && files) {
-			setForm({ ...form, [name]: files[0] });
-		} else {
-			setForm({ ...form, [name]: value });
-		}
-	};
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
+    const { name, value, type, files } = e.target as HTMLInputElement;
 
-	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		const element = e.target as HTMLFormElement;
-		const target = element.querySelector(
-			"button[type=submit]"
-		) as HTMLButtonElement;
+    if (type === "file" && files) {
+      setForm({ ...form, [name]: files[0] });
+    } else {
+      setForm({ ...form, [name]: value });
+    }
+  };
 
-		target.disabled = true;
-		target.classList.add("bg-gray-500");
-		target.classList.remove("bg-green-600");
-		const buttonText = target.innerText;
-		target.innerText = "Submitting...";
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const element = e.target as HTMLFormElement;
+    const target = element.querySelector(
+      "button[type=submit]"
+    ) as HTMLButtonElement;
 
-		setError("");
-		setSuccess("");
+    target.disabled = true;
+    target.classList.add("bg-gray-500");
+    target.classList.remove("bg-green-600");
+    const buttonText = target.innerText;
+    target.innerText = "Submitting...";
 
-		for (const field of fields) {
-			if (!form[field.name]) {
-				if (field?.required === false) {
-					continue;
-				}
-				setError(`${field.label} cannot be empty!`);
-				target.disabled = false;
-				target.classList.remove("bg-gray-500");
-				target.classList.add("bg-red-600");
-				target.innerText = buttonText;
-				return;
-			}
-		}
-		await submit(form);
-		target.disabled = false;
-		target.classList.remove("bg-gray-500");
-		target.classList.add("bg-green-600");
-		target.innerText = buttonText;
+    setError("");
+    setSuccess("");
 
-		setSuccess("success!");
-	};
+    for (const field of fields) {
+      if (!form[field.name]) {
+        if (field?.required === false) {
+          continue;
+        }
+        setError(`${field.label} cannot be empty!`);
+        target.disabled = false;
+        target.classList.remove("bg-gray-500");
+        target.classList.add("bg-red-600");
+        target.innerText = buttonText;
+        return;
+      }
+    }
+    
+    await submit(form);
+    target.disabled = false;
+    target.classList.remove("bg-gray-500");
+    target.classList.add("bg-green-600");
+    target.innerText = buttonText;
 
-	return (
-		<div className="base-form shadow-md p-4 rounded-md  bg-gray-900 text-white">
-			<h1 className="text-xl font-black font-mono border-b pb-2 border-blue-200">
-				{title}
-			</h1>
-			<form onSubmit={handleSubmit} className="flex flex-col gap-4">
-				{fields.map((field: Field) => (
-					<div key={field.name} className="flex flex-col gap-2">
-						<div className="flex justify-between items-center mt-2">
-							<label className="text-lg" htmlFor={field.name}>
-								{field.label}
-							</label>
-							{/* Input Fields */}
-							{field.type === "select" ? (
-								<select
-									name={field.name}
-									value={form[field.name] || field.value || ""}
-									onChange={handleChange}
-									className="p-2 text-black w-full md:w-2/3 rounded-md "
-								>
-									<option value="">Select...</option>
-									{field.options?.map((option: string) => (
-										<option key={option.replace(" ", "-")} value={option}>
-											{option}
-										</option>
-									))}
-								</select>
-							) : field.type === "textarea" ? (
-								<textarea
-									name={field.name}
-									value={form[field.name] || field.value || ""}
-									onChange={handleChange}
-									placeholder={field.placeholder}
-									className="p-2 text-black w-full md:w-2/3 rounded-md"
-								/>
-							) : field.type === "file" ? (
-								<input
-									type="file"
-									name={field.name}
-									onChange={handleChange}
-									className="p-2 text-black w-full md:w-2/3 rounded-md"
-								/>
-							) : (
-								<input
-									type={field.type}
-									name={field.name}
-									value={form[field.name] || field.value || ""}
-									onChange={handleChange}
-									placeholder={field.placeholder}
-									className="p-2 text-black w-full md:w-2/3 rounded-md"
-								/>
-							)}
-						</div>
-					</div>
-				))}
-				<button
-					type="submit"
-					className="w-fit bg-green-600 hover:bg-green-800 text-white font-bold px-2 text-xl rounded"
-				>
-					{submitText}
-				</button>
-			</form>
-			{error && <p className="text-red-500">{error}</p>}
-			{success && <p className="text-green-500">{success}</p>}
-		</div>
-	);
+    setSuccess("success!");
+  };
+
+  return (
+    <div className="base-form shadow-md p-4 rounded-md bg-gray-900 text-white">
+      <h1 className="text-xl font-black font-mono border-b pb-2 border-blue-200">
+        {title}
+      </h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        {fields.map((field: Field) => (
+          <div key={field.name} className="flex flex-col gap-2">
+            <div className="flex justify-between items-center mt-2">
+              <label className="text-lg" htmlFor={field.name}>
+                {field.label}
+              </label>
+              {field.type === "select" ? (
+                <select
+                  name={field.name}
+                  value={form[field.name] || ""}
+                  onChange={handleChange}
+                  className="p-2 text-black w-full md:w-2/3 rounded-md"
+                >
+                  <option value="">Select...</option>
+                  {field.options?.map((option: string) => (
+                    <option key={option.replace(" ", "-")} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              ) : field.type === "textarea" ? (
+                <textarea
+                  name={field.name}
+                  value={form[field.name] || ""}
+                  onChange={handleChange}
+                  placeholder={field.placeholder}
+                  className="p-2 text-black w-full md:w-2/3 rounded-md"
+                />
+              ) : field.type === "file" ? (
+                <input
+                  type="file"
+                  name={field.name}
+                  onChange={handleChange}
+                  className="p-2 text-black w-full md:w-2/3 rounded-md"
+                />
+              ) : (
+                <input
+                  type={field.type}
+                  name={field.name}
+                  value={form[field.name] || ""}
+                  onChange={handleChange}
+                  placeholder={field.placeholder}
+                  className="p-2 text-black w-full md:w-2/3 rounded-md"
+                />
+              )}
+            </div>
+          </div>
+        ))}
+        <button
+          type="submit"
+          className="w-fit bg-green-600 hover:bg-green-800 text-white font-bold px-2 text-xl rounded"
+        >
+          {submitText}
+        </button>
+      </form>
+      {error && <p className="text-red-500">{error}</p>}
+      {success && <p className="text-green-500">{success}</p>}
+    </div>
+  );
 };
